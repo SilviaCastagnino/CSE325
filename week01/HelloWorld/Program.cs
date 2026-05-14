2fa86e85d50786deffa64c7135d4785c14eec36d
SilviaCastagnino <sylvia19976@gmail.com>
2026-05-14 17:59:46 +0200

    New files

---
+++ week01/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+var date = DateTime.Now;
+
+DateTime today = DateTime.Today;
+DateTime nextChristmas = new DateTime(today.Year, 12, 25);
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {date:MM-dd-yyyy} {date:HH:mm:ss}");
+Console.WriteLine($"There are {(nextChristmas - today).Days} days until christmas");
